1e47fee74e92aebc21e22959965119e570b1e9e6
E94111164 <113426842+E94111164@users.noreply.github.com>
2022-10-04 20:48:13 +0800

    10040848

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -3,12 +3,15 @@ B = []
 C = []
 
 print("開始輸入A同學的成績，請依照 國文、英文、數學、自然、社會 的順序輸入:")
+
+#利用for迴圈來輸入成績
 for i in range(0, 5):
   score = input()
   A.append(score)
 print("A學生成績: ")
 print("國文: ", A[0], "、英文: ", A[1], "、數學: ", A[2], "、自然: ", A[3], "、社會: ", A[4])
 
+print(" ")
 print("開始輸入B同學的成績，請依照 國文、英文、數學、自然、社會 的順序輸入:")
 for i in range(0, 5):
   score = input()
@@ -16,6 +19,7 @@ for i in range(0, 5):
 print("B學生成績: ")
 print("國文: ", B[0], "、英文: ", B[1], "、數學: ", B[2], "、自然: ", B[3], "、社會: ", B[4])
 
+print(" ")
 print("開始輸入C同學的成績，請依照 國文、英文、數學、自然、社會 的順序輸入:")
 for i in range(0, 5):
   score = input()
@@ -23,10 +27,12 @@ for i in range(0, 5):
 print("C學生成績: ")
 print("國文: ", C[0], "、英文: ", C[1], "、數學: ", C[2], "、自然: ", C[3], "、社會: ", C[4])
 
+#利用變數計算平均
 sumA = 0
 sumB = 0
 sumC = 0
 
+#利用迴圈計算平均
 for i in range(0, 5):
   sumA = sumA + int(A[i])
 print("A同學平均成績: ", sumA/5)
@@ -45,8 +51,9 @@ mathsum = int(A[2]) + int(B[2]) + int(C[2])
 scisum = int(A[3]) + int(B[3]) + int(C[3])
 socsum = int(A[4]) + int(B[4]) + int(C[4])
 
+print(" ")
 print("國文平均成績: ", chisum/3)
 print("英文平均成績: ", engsum/3)
 print("數學平均成績: ", mathsum/3)
 print("自然平均成績: ", scisum/3)
-print("社會平均成績: ", scosum/3)
+print("社會平均成績: ", socsum/3)
