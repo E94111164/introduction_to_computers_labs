9fdc5df5cc2d82e875ebb6564f5cb7341b9f4957
E94111164 <113426842+E94111164@users.noreply.github.com>
2022-10-24 01:01:16 +0800

    10240101

---
+++ lab_07/lab_07.py
@@ -0,0 +1,53 @@
+class Animal():
+    def __init__(self, weight, mood):
+      self.weight = weight
+      self.mood = mood
+    def feed(self):
+        pass
+    def walk(self):
+        pass
+    def bath(self):
+        pass
+    
+class Dogs(Animal):
+    def __init__(self, weight, mood):
+        self.weight = weight
+        self.mood = mood
+    def feed(self, n_feed):
+        self.weight = self.weight + n_feed*0.2
+        self.mood = self.mood + n_feed*1
+    def walk(self, n_walk):
+        self.weight = self.weight - n_walk*0.2
+        self.mood = self.mood + n_walk*2
+    def bath(self, n_bath):
+        self.mood = self.mood - n_bath*2
+    def printf(self, n_feed, n_walk, n_bath):
+        self.feed(n_feed)
+        self.walk(n_walk)
+        self.bath(n_bath)
+        print("狗狗現在的體重=", self.weight, "kg", "心情=", self.mood) 
+
+
+class Cats(Animal):
+    def __init__(self, weight, mood):
+      self.weight = weight
+      self.mood = mood
+    def feed(self,n_feed):
+      self.weight = self.weight + n_feed*0.1
+      self.mood = self.mood + n_feed*1
+    def walk(self, n_walk):
+      self.weight = self.weight - n_walk*0.1
+      self.mood = self.mood - n_walk*1
+    def bath(self, n_bath):
+      self.mood = self.mood - n_bath*2
+    def printf(self, n_feed, n_walk, n_bath):
+      self.feed(n_feed)
+      self.walk(n_walk)
+      self.bath(n_bath)
+      print("貓貓現在的體重=", self.weight, "kg", "心情=", self.mood) 
+
+dog = Dogs(4.8, 65) 
+dog.printf(18, 10, 4) 
+
+cat = Cats(8.2, 60) 
+cat.printf(40, 7, 1) 
