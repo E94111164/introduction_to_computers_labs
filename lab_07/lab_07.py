8adca1a7403f7d4a74e0fcc63d7cfccfbbe9e3ef
E94111164 <113426842+E94111164@users.noreply.github.com>
2022-10-24 20:19:18 +0800

    10240819

--- lab_07/lab_07.py
+++ lab_07/lab_07.py
@@ -1,14 +1,18 @@
+#建立Animal的類別
 class Animal():
     def __init__(self, weight, mood):
       self.weight = weight
       self.mood = mood
     def feed(self):
+        #先跳過feed和walk的函式
         pass
     def walk(self):
         pass
     def bath(self, n_bath):
+        #因為貓狗的bath一樣，因此先寫bath的函式
         self.mood = self.mood -2*n_bath
-    
+
+#建立Dogs子類別
 class Dogs(Animal):
     def __init__(self, weight, mood):
         self.weight = weight
@@ -20,6 +24,7 @@ class Dogs(Animal):
         self.weight = self.weight - n_walk*0.2
         self.mood = self.mood + n_walk*2
     def bath(self, n_bath):
+        #繼承父類別的函式
         super().bath(n_bath)
     def printf(self, n_feed, n_walk, n_bath):
         self.feed(n_feed)
@@ -27,7 +32,7 @@ class Dogs(Animal):
         self.bath(n_bath)
         print("狗狗現在的體重=", self.weight, "kg", "心情=", self.mood) 
 
-
+#建立Cats子類別
 class Cats(Animal):
     def __init__(self, weight, mood):
       self.weight = weight
@@ -39,6 +44,7 @@ class Cats(Animal):
       self.weight = self.weight - n_walk*0.1
       self.mood = self.mood - n_walk*1
     def bath(self, n_bath):
+      #繼承父類別的函式
       super().bath(n_bath)
     def printf(self, n_feed, n_walk, n_bath):
       self.feed(n_feed)
